045c499e64e4f5abefb1a1825f9598ee9a452ee9
SeungMin Lee <lsm.somniator@gmail.com>
2020-10-24 21:20:13 +0900

    Make Slack available; Add Slack tests

---
+++ bwa/send_slack.py
@@ -0,0 +1,90 @@
+# Simple Notification Sender for Slack
+# Author: SeungMin Lee,
+# 		  Software Developer at ReturnZero Inc.,
+#		  Undergraduate Student at DGIST.
+# Reference: Hugging Face's knockknock
+# 			 https://github.com/huggingface/knockknock
+
+from datetime import datetime
+import functools
+import json
+import os
+import requests
+import traceback
+
+DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+
+class WebhookUrlEnteredError(Exception):
+    def __init__(self):
+        super().__init__("No webhook url entered; Please make sure that you entered `webhook_url` or the environment variable `SLACK_WEBHOOK_URL` correctly.")
+
+
+def deco_noti(webhook_url="", custom_content="", notify_end_too=False):
+    def get_content_for_start(fname, start):
+        return "🏃 Your function <{fname}> has started.\n \
+		\t- function name: {fname}\n \
+		\t- start time: {str_start}".format(fname=fname,
+                                      str_start=start.strftime(DATE_FORMAT))
+
+    def get_content_for_end(fname, start, end, run):
+        return "🎉 Your function <{fname}> is complete!\n \
+		\t- function name: {fname}\n \
+		\t- start time: {str_start}\n \
+		\t- end time: {str_end}\n \
+		\t- run time: {run}".format(fname=fname, str_start=start.strftime(DATE_FORMAT),
+                              str_end=end.strftime(DATE_FORMAT), run=run)
+
+    def get_content_for_dead(fname, start, end, run, exp):
+        return "😭 Your function <{fname}> died in action...\n \
+		\t- function name: {fname}\n \
+		\t- start time: {str_start}\n \
+		\t- dead time: {str_end}\n \
+		\t- run time: {run}\n \
+		\t- Error Info: {exp}\n\n \
+		\t- Traceback:\n \
+		\t{trace}".format(fname=fname, str_start=start.strftime(DATE_FORMAT),
+                    str_end=end.strftime(DATE_FORMAT), run=run,
+                    exp=exp, trace=traceback.format_exc())
+
+    def decorator(func):
+        url = webhook_url or os.environ.get('SLACK_WEBHOOK_URL')
+
+        if not url:
+            raise WebhookUrlEnteredError()
+
+        def send(contents):
+            requests.post(url, json.dumps(
+                {'text': custom_content or contents}))
+
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            start = datetime.now()
+            fname = func.__name__
+
+            send(get_content_for_start(fname, start))
+
+            if notify_end_too and not custom_content:
+                try:
+                    _ = func(*args, kwargs)
+                    end = datetime.now()
+
+                    send(get_content_for_end(fname, start, end, end - start))
+                except Exception as exp:
+                    end = datetime.now()
+                    send(get_content_for_dead(
+                        fname, start, end, end - start, exp))
+
+        return wrapper
+    return decorator
+
+
+def send_noti(webhook_url="", custom_content=""):
+    url = webhook_url or os.environ.get('SLACK_WEBHOOK_URL')
+    if not url:
+        raise WebhookUrlEnteredError()
+
+    default = "The notification has arrived from your program."
+
+    _ = requests.post(url=url, data=json.dumps(
+        {'content': custom_content or default}), headers={'Content-Type': 'application/json'})
---
+++ bwa/test/test_slack.py
@@ -0,0 +1,69 @@
+# This test may be executed as follows:
+# $ python -m unittest discover -v bwa.test
+
+from time import sleep
+import unittest
+
+from bwa.send_slack import deco_noti, send_noti
+
+webhook_url = "<YOUR_WEBHOOK_URL>"
+
+
+class SlackTest(unittest.TestCase):
+    def test_decorator(self):
+        @deco_noti(webhook_url=webhook_url)
+        def decorator():
+            sleep(3)
+        decorator()
+
+    def test_decorator_with_end(self):
+        @deco_noti(webhook_url=webhook_url, notify_end_too=True)
+        def decorator_with_end():
+            sleep(3)
+        decorator_with_end()
+
+    def test_decorator_with_exception(self):
+        @deco_noti(webhook_url=webhook_url, notify_end_too=True)
+        def decorator_with_exception():
+            sleep(3)
+            raise Exception
+        decorator_with_exception()
+
+    def test_decorator_with_custom_content(self):
+        @deco_noti(webhook_url=webhook_url, custom_content="Hello, Slack")
+        def decorator_with_custom_content():
+            sleep(3)
+        decorator_with_custom_content()
+
+    def test_decorator_with_custom_content_true(self):
+        @deco_noti(webhook_url=webhook_url, custom_content="Hello, Slack", notify_end_too=True)
+        def decorator_with_custom_content_true():
+            sleep(3)
+        decorator_with_custom_content_true()
+
+    def test_function(self):
+        send_noti(webhook_url=webhook_url, custom_content="Hello, Slack")
+
+    # Enter enviroment variable
+    # os.environ['SLACK_WEBHOOK_URL'] = webhook_url
+    def test_decorator_with_envar(self):
+        @deco_noti()
+        def decorator_with_envar():
+            sleep(3)
+        decorator_with_envar()
+
+    def test_simple_decorator_with_custom_content(self):
+        @deco_noti(custom_content="Hello, Slack")
+        def simple_decorator_with_custom_content():
+            sleep(3)
+        simple_decorator_with_custom_content()
+
+    def test_function_with_envar(self):
+        send_noti()
+
+    def test_simple_function_with_custom_content(self):
+        send_noti(custom_content="Hello, Slack")
+
+
+if __name__ == "__main__":
+    unittest.main()
