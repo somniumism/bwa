441663f942df777d2c8dc4767299ebf23505e2f8
SeungMin Lee <lsm.somniator@gmail.com>
2020-10-24 19:43:05 +0900

    Make Gmail available; Add Gmail tests

---
+++ bwa/send_gmail.py
@@ -0,0 +1,117 @@
+# Notification Sender for Gmail
+# Author: SeungMin Lee,
+# 		  Software Developer at ReturnZero Inc.,
+#		  Undergraduate Student at DGIST.
+# Reference: Hugging Face's knockknock
+# 			 https://github.com/huggingface/knockknock
+
+from datetime import datetime
+import functools
+import json
+import os
+import requests
+import traceback
+import yagmail
+from smtplib import SMTPAuthenticationError
+
+DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+
+class UserInfoEnteredError(Exception):
+    def __init__(self):
+        super().__init__("Your email info is invalid; Please make sure that you entered `sender_email` and 'sender_password' or the environment variable `SENDER_EMAIL` and `SENDER_PASSWORD` correctly.")
+
+
+class AuthenticationError(Exception):
+    def __init__(self):
+        super().__init__("Your account authentication failed; Please check your email address and password correctly.")
+
+
+def deco_noti(receiver_emails=[], sender_email="", sender_password="", custom_content="", notify_end_too=False):
+    user = sender_email or os.environ.get('SENDER_EMAIL')
+    password = sender_password or os.environ.get('SENDER_PASSWORD')
+    if not user or not password:
+        raise UserInfoEnteredError()
+    sender = yagmail.SMTP(user=user, password=password)
+    receivers = receiver_emails or [user]
+
+    def get_content_for_start(fname, start):
+        return "🏃 Your function <{fname}> has started.\n \
+		\t- function name: {fname}\n \
+		\t- start time: {str_start}".format(fname=fname,
+                                      str_start=start.strftime(DATE_FORMAT))
+
+    def get_content_for_end(fname, start, end, run):
+        return "🎉 Your function <{fname}> is complete!\n \
+		\t- function name: {fname}\n \
+		\t- start time: {str_start}\n \
+		\t- end time: {str_end}\n \
+		\t- run time: {run}".format(fname=fname, str_start=start.strftime(DATE_FORMAT),
+                              str_end=end.strftime(DATE_FORMAT), run=run)
+
+    def get_content_for_dead(fname, start, end, run, exp):
+        return "😭 Your function <{fname}> died in action...\n \
+		\t- function name: {fname}\n \
+		\t- start time: {str_start}\n \
+		\t- dead time: {str_end}\n \
+		\t- run time: {run}\n \
+		\t- Error Info: {exp}\n\n \
+		\t- Traceback:\n \
+		\t{trace}".format(fname=fname, str_start=start.strftime(DATE_FORMAT),
+                    str_end=end.strftime(DATE_FORMAT), run=run,
+                    exp=exp, trace=traceback.format_exc())
+
+    def decorator(func):
+        def send(contents, receiver):
+            try:
+                content = custom_content or contents
+                title = content.split("\n")[0][:50] if len(
+                    content.split("\n")[0]) > 50 else content.split("\n")[0]
+                sender.send(receiver, title, content)
+            except SMTPAuthenticationError:
+                raise AuthenticationError()
+
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            start = datetime.now()
+            fname = func.__name__
+
+            for receiver in receivers:
+                send(get_content_for_start(fname, start), receiver)
+
+            if notify_end_too and not custom_content:
+                try:
+                    _ = func(*args, **kwargs)
+                    end = datetime.now()
+
+                    for receiver in receivers:
+                        send(get_content_for_end(
+                            fname, start, end, end - start), receiver)
+
+                except Exception as exp:
+                    end = datetime.now()
+                    for receiver in receivers:
+                        send(get_content_for_dead(fname, start,
+                                                  end, end - start, exp), receiver)
+                    raise exp
+
+        return wrapper
+    return decorator
+
+
+def send_noti(receiver_emails=[], sender_email="", sender_password="", custom_content=""):
+    user = sender_email or os.environ.get('SENDER_EMAIL')
+    password = sender_password or os.environ.get('SENDER_PASSWORD')
+    if not user or not password:
+        raise UserInfoEnteredError()
+    sender = yagmail.SMTP(user=user, password=password)
+    receivers = receiver_emails or [user]
+
+    content = custom_content or "The notification has arrived from your program."
+    title = content.split("\n")[0][:50] if len(
+        content.split("\n")[0]) > 50 else content.split("\n")[0]
+    try:
+        for receiver in receivers:
+            sender.send(receiver, title, content)
+    except SMTPAuthenticationError:
+        raise AuthenticationError()
---
+++ bwa/test/test_gmail.py
@@ -0,0 +1,91 @@
+# This test may be executed as follows:
+# $ python -m unittest discover -v bwa.test.test_email
+
+from time import sleep
+import unittest
+
+from bwa.send_gmail import deco_noti, send_noti
+
+receiver_emails = ["<GMAIL_ADDRESS>"]
+sender_email = "<TESTER's EMAIL>"
+sender_password = "<TESTER's PASSWORD>"
+
+
+class GmailTest(unittest.TestCase):
+    def test_gmail(self):
+        @deco_noti(receiver_emails=receiver_emails, sender_email=sender_email, sender_password=sender_password)
+        def decorator():
+            sleep(3)
+        decorator()
+
+    def test_decorator_with_end(self):
+        @deco_noti(receiver_emails=receiver_emails, sender_email=sender_email, sender_password=sender_password, notify_end_too=True)
+        def decorator_with_end():
+            sleep(3)
+        decorator_with_end()
+
+    def test_decorator_with_exception(self):
+        @deco_noti(receiver_emails=receiver_emails, sender_email=sender_email, sender_password=sender_password, notify_end_too=True)
+        def decorator_with_exception():
+            sleep(3)
+            raise Exception
+        decorator_with_exception()
+
+    def test_decorator_with_custom_content(self):
+        @deco_noti(receiver_emails=receiver_emails, sender_email=sender_email, sender_password=sender_password, custom_content="Hello, Gmail")
+        def decorator_with_custom_content():
+            sleep(3)
+        decorator_with_custom_content()
+
+    def test_decorator_with_custom_content_true(self):
+        @deco_noti(receiver_emails=receiver_emails, sender_email=sender_email, sender_password=sender_password, custom_content="Hello, Gmail", notify_end_too=True)
+        def decorator_with_custom_content_true():
+            sleep(3)
+        decorator_with_custom_content_true()
+
+    def test_function(self):
+        send_noti(receiver_emails=receiver_emails, sender_email=sender_email,
+                  sender_password=sender_password, custom_content="Hello, Gmail")
+
+    def test_decorator_with_no_receiver_emails(self):
+        @deco_noti(sender_email=sender_email, sender_password=sender_password)
+        def decorator_with_no_receiver_emails():
+            sleep(3)
+        decorator_with_no_receiver_emails()
+
+    def test_function_with_no_receiver_emails(self):
+        send_noti(sender_email=sender_email,
+                  sender_password=sender_password, custom_content="Hello, Gmail")
+
+    # Enter enviroment variable
+    # os.environ['SENDER_EMAIL'] = sender_email
+    # os.environ['SENDER_PASSWORD'] = sender_password
+    # or `$ export SENDER_EMAIL="<SENDER_EMAIL>"`
+    #    `$ export SENDER_PASSWORD="<SENDER_PASSWORD>"`
+    def test_decorator_with_envar(self):
+        @deco_noti(receiver_emails=receiver_emails)
+        def decorator_with_envar(receiver_emails=receiver_emails):
+            sleep(3)
+        decorator_with_envar()
+
+    def test_simple_decorator(self):
+        @deco_noti()
+        def simple_decorator():
+            sleep(3)
+        simple_decorator()
+
+    def test_simple_decorator_with_custom_content(self):
+        @deco_noti(receiver_emails=receiver_emails, custom_content="Hello, Gmail")
+        def simple_decorator_with_custom_content():
+            sleep(3)
+        simple_decorator_with_custom_content()
+
+    def test_function_with_envar(self):
+        send_noti()
+
+    def test_simple_function_with_custom_content(self):
+        send_noti(custom_content="Hello, Gmail")
+
+
+if __name__ == "__main__":
+    unittest.main()
